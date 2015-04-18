4d57d54a52a8d2dd6c6bd99c4fd5f21a7e376b1b
Miguel Garcia <miguelgarcia2515@gmail.com>
2015-04-17 20:12:17 -0700

    1

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Simple To-Do list </title>
+		<link rel="stylesheet" type="text/css" media="screen" href="main.css">
+	</head>
+	<body>
+	<div class="wrap">
+		
+	</div>
+	</body>
+</html>
